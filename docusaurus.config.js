6ffd60cf335d5a411dda00a5ebef4dcd3c936441
wildone <max.barrass@gmail.com>
2026-03-11 00:39:34 +1100

    fix: set custom-domain baseUrl to root

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/vibegov-icon.svg',
 
   // Set the production url of your site here
-  url: 'https://governance-foundation.github.io',
+  url: 'https://vibegov.io',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/vibegov.io/',
+  // For custom domain deployment, this should be '/'
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
